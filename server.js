25fb7a6690d87bd34affa1d43754c2d2b6889914
NSHUTI <nshkp@gmail.com>
2026-04-13 08:21:31 +0200

    m

--- server.js
+++ server.js
@@ -933,7 +933,6 @@ const upload = multer({
 app.use("/uploads", express.static("uploads"));
 
 // ✅ JWT authentication middleware
-import jwt from "jsonwebtoken";
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
